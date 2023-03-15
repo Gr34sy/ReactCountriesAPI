d3ec61a4974b8e765becc9bf35d178d5af657997
Gr34sy <slupek3@poczta.fm>
2023-03-15 14:31:28 +0100

    added navbar, basic styling and scss reset

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+export function Header(){
+    // useEffect(() => {
+    //     document.querySelector('body').classList.add('darkmode');
+    // },[])
+
+    return(
+        <header className="header">
+            <h1 className="header__title">Where in the world?</h1>
+            <button>
+                test
+            </button>
+        </header>
+    )
+}
