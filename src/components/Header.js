be1f777af5fa7116de3895b848f8353e5295cb85
Gr34sy <slupek3@poczta.fm>
2023-03-15 15:04:06 +0100

    added switch mode button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,18 @@ import {  Outlet, Link } from "react-router-dom";
 
 export function Header(){
     // useEffect(() => {
-    //     document.querySelector('body').classList.add('darkmode');
+    //     document.querySelector('body').classList.toggle('darkmode');
     // },[])
 
     return(
         <>
         <header className="header">
             <Link to="/"><h1 className="header__title">Where in the world?</h1></Link>
-            <button className="darkmode-switch">
-                test
+            <button className="darkmode-switch" onClick={() => document.querySelector('body').classList.toggle('darkmode')}>
+                switch
             </button>
         </header>
-        
+
         <Outlet/>
         </>
     )
